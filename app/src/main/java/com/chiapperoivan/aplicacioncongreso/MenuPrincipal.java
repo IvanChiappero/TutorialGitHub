e3a1f8c3475ced3a513d8a2689e226e630837e18
IvanChiappero <chiapperoivan@gmail.com>
2018-05-13 01:08:39 -0300

    Crashlytics

--- app/src/main/java/com/chiapperoivan/aplicacioncongreso/MenuPrincipal.java
+++ app/src/main/java/com/chiapperoivan/aplicacioncongreso/MenuPrincipal.java
@@ -3,11 +3,21 @@ package com.chiapperoivan.aplicacioncongreso;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Addition6: Plugin Crashlytics Fabric
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+//Addition6 Fin
+
 public class MenuPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Addition7: Plugin Crashlytics Fabric
+        Fabric.with(this, new Crashlytics());
+        //Addition7 Fin
+
         setContentView(R.layout.activity_menu_principal);
     }
 }
